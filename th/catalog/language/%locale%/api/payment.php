bb1197b8283d7c585799eeffc03bc37817438a17
tazysnow <turtaew69@gmail.com>
2018-02-06 03:42:38 +0700

    api payment translated

--- th/catalog/language/%locale%/api/payment.php
+++ th/catalog/language/%locale%/api/payment.php
@@ -1,18 +1,18 @@
 <?php
 // Text
-$_['text_address']       = 'Success: Payment address has been set!';
-$_['text_method']        = 'Success: Payment method has been set!';
+$_['text_address']       = 'สำเร็จ: ได้ตั้งค่าที่อยู่การชำระเงินแล้ว!';
+$_['text_method']        = 'ความสำเร็จ: ได้ตั้งค่าวิธีการชำระเงินแล้ว!';
 
 // Error
-$_['error_permission']   = 'Warning: You do not have permission to access the API!';
-$_['error_firstname']    = 'First Name must be between 1 and 32 characters!';
-$_['error_lastname']     = 'Last Name must be between 1 and 32 characters!';
-$_['error_address_1']    = 'Address 1 must be between 3 and 128 characters!';
-$_['error_city']         = 'City must be between 3 and 128 characters!';
-$_['error_postcode']     = 'Postcode must be between 2 and 10 characters for this country!';
-$_['error_country']      = 'Please select a country!';
-$_['error_zone']         = 'Please select a region / state!';
-$_['error_custom_field'] = '%s required!';
-$_['error_address']      = 'Warning: Payment address required!';
-$_['error_method']       = 'Warning: Payment method required!';
-$_['error_no_payment']   = 'Warning: No Payment options are available!';
+$_['error_permission']   = 'คำเตือน: คุณไม่มีสิทธิ์เข้าถึง API!';
+$_['error_firstname']    = 'ชื่อจริง ต้องมีความยาวระหว่าง 1 ถึง 32 ตัวอักษร!!';
+$_['error_lastname']     = 'นามสกุล ต้องมีความยาวระหว่าง 1 ถึง 32 ตัวอักษร!';
+$_['error_address_1']    = 'ที่อยู่ 1 ต้องมีความยาวระหว่าง 3 ถึง 128 ตัวอักษร!';
+$_['error_city']         = 'เมือง ต้องมีความยาวระหว่าง 3 ถึง 128 ตัวอักษร!';
+$_['error_postcode']     = 'รหัสไปรษณีย์จำเป็นต้องมีความยาวระหว่าง 2 ถึง 10 ตัวอักษร!';
+$_['error_country']      = 'กรุณาเลือกประเทศ!';
+$_['error_zone']         = 'กรุณาเลือกพื้นที่ หรือเขต!';
+$_['error_custom_field'] = '%s จำเป็น!';
+$_['error_address']      = 'คำเตือน: จำเป็นต้องใช้ที่อยู่ในการชำระเงิน!';
+$_['error_method']       = 'คำเตือน: ในการชำระเงินจำเป็นต้องเลือกวิธีการจัดส่ง!';
+$_['error_no_payment']   = 'คำเตือน: ตัวเลือกการชำระเงินไม่มี';
