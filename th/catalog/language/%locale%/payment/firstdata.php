ddc6a7ff92dcb1a0cb239450c2814b0c11fbae8c
tazysnow <turtaew69@gmail.com>
2018-02-06 21:37:33 +0700

    extension/payment

--- th/catalog/language/%locale%/payment/firstdata.php
+++ th/catalog/language/%locale%/payment/firstdata.php
@@ -1,42 +1,42 @@
 <?php
 // Heading
-$_['text_title']				= 'Credit / Debit card (First Data)';
+$_['text_title']				= 'บัตรเครดิต / บัตรเดบิต(ข้อมูลเริ่มแรก)';
 
 // Button
-$_['button_confirm']			= 'Continue';
+$_['button_confirm']			= 'ดำเนินการต่อ';
 
 // Text
-$_['text_new_card']				= 'New card';
-$_['text_store_card']			= 'Remember my card details';
-$_['text_address_response']		= 'Address verification: ';
-$_['text_address_ppx']			= 'No address data provided or Address not checked by the Card Issuer';
-$_['text_address_yyy']			= 'Card Issuer confirmed that street and postcode match with their records';
-$_['text_address_yna']			= 'Card Issuer confirmed that street matches with their records but postcode does not match';
-$_['text_address_nyz']			= 'Card Issuer confirmed that postcode matches with their records but street does not match';
-$_['text_address_nnn']			= 'Both street and postcode do not match with the Card Issuer records';
-$_['text_address_ypx']			= 'Card Issuer confirmed that street matches with their records. The Issuer did not check the postcode';
-$_['text_address_pyx']			= 'Card Issuer confirmed that postcode matches with their records. The Issuer did not check the street';
-$_['text_address_xxu']			= 'Card Issuer did not check the AVS information';
-$_['text_card_code_verify']		= 'Security code: ';
-$_['text_card_code_m']			= 'Card security code match';
-$_['text_card_code_n']			= 'Card security code does not match';
-$_['text_card_code_p']			= 'Not processed';
-$_['text_card_code_s']			= 'Merchant has indicated that the card security code is not present on the card';
-$_['text_card_code_u']			= 'Issuer is not certified and/or has not provided encryption keys';
-$_['text_card_code_x']			= 'No response from the credit card association was received';
-$_['text_card_code_blank']		= 'A blank response should indicate that no code was sent and that there was no indication that the code was not present on the card.';
-$_['text_card_type_m']			= 'Mastercard';
-$_['text_card_type_v']			= 'Visa (Credit/Debit/Electron/Delta)';
+$_['text_new_card']				= 'บัตรใหม่';
+$_['text_store_card']			= 'จดจำรายละเอียดของบัตร';
+$_['text_address_response']		= 'ยืนยันที่อยู่: ';
+$_['text_address_ppx']			= 'ไม่มีข้อมูลที่อยู่หรือที่อยู่ไม่ได้รับการตรวจสอบจากผู้ออกบัตร';
+$_['text_address_yyy']			= 'ผู้ออกบัตรยืนยันว่าถนนและรหัสไปรษณีย์ตรงกับบันทึกของพวกเขา';
+$_['text_address_yna']			= 'ผู้ออกบัตรยืนยันว่าถนนตรงกับบันทึก แต่รหัสไปรษณีย์ไม่ตรงกับบันทึกของพวกเขา';
+$_['text_address_nyz']			= 'ผู้ออกบัตรยืนยันว่าหัสไปรษณีย์ตรงกับบันทึก แต่ถนนไม่ตรงกับบันทึกของพวกเขา';
+$_['text_address_nnn']			= 'ถนนและรหัสไปรษณีย์ไม่ตรงกับบันทึกของผู้ออกบัตร';
+$_['text_address_ypx']			= 'ผู้ออกบัตรยืนยันว่าถนนตรงกันกับบันทึกของพวกเขา ผู้ออกบัตรไม่ตรวจสอบรหัสไปรษณีย์';
+$_['text_address_pyx']			= 'ผู้ออกบัตรยืนยันว่ารหัสไปรษณีย์ตรงกันกับบันทึกของพวกเขา ผู้ออกบัตรไม่ตรวจสอบถนน';
+$_['text_address_xxu']			= 'ผู้ออกบัตรไม่ได้ตรวจสอบข้อมูล AVS';
+$_['text_card_code_verify']		= 'รหัสความปลอดภัย: ';
+$_['text_card_code_m']			= 'รหัสบัตรรักษาความปลอดภัยตรงกัน';
+$_['text_card_code_n']			= 'รหัสบัตรรักษาความปลอดภัยไม่ตรงกัน';
+$_['text_card_code_p']			= 'ไม่ได้ประมวลผล';
+$_['text_card_code_s']			= 'ผู้ขายระบุว่ารหัสบัตรรักษาความปลอดภัยไม่มีอยู่ในการ์ด';
+$_['text_card_code_u']			= 'ผู้ออกบัตรไม่ได้รับการรับรองและ / หรือไม่ได้ระบุคีย์การเข้ารหัส';
+$_['text_card_code_x']			= 'ไม่ได้รับการตอบกลับจากบริษัทบัตรเครดิต';
+$_['text_card_code_blank']		= 'การตอบกลับที่ว่างเปล่า ควรระบุว่าไม่มีการส่งรหัสและไม่มีข้อบ่งชี้ว่ารหัสไม่ได้อยู่ในบัตร';
+$_['text_card_type_m']			= 'มาสเตอร์การ์ด';
+$_['text_card_type_v']			= 'วีซ่า (เครดิต / เดบิต / อิเล็กตรอน / เดลต้า)';
 $_['text_card_type_c']			= 'Diners';
 $_['text_card_type_a']			= 'American Express';
 $_['text_card_type_ma']			= 'Maestro';
 $_['text_card_type_mauk']		= 'Maestro UK/Solo';
-$_['text_response_code_full']	= 'Approval code: ';
-$_['text_response_code']		= 'Full response code: ';
-$_['text_response_card']		= 'Card used: ';
-$_['text_response_card_type']	= 'Card type: ';
-$_['text_response_proc_code']	= 'Processor code: ';
-$_['text_response_ref']			= 'Ref number: ';
+$_['text_response_code_full']	= 'รหัสอนุมัติ:';
+$_['text_response_code']		= 'รหัสการตอบสนองแบบเต็ม: ';
+$_['text_response_card']		= 'บัตรที่ใช้:';
+$_['text_response_card_type']	= 'ประเภทของบัตร:';
+$_['text_response_proc_code']	= 'รหัสประมวลผล:';
+$_['text_response_ref']			= 'หมายเลขอ้างอิง:';
 
 // Error
-$_['error_failed']				= 'Unable to process your payment, please try again';
+$_['error_failed']				= 'ไม่สามารถประมวลผลการชำระเงินของคุณได้ โปรดลองอีกครั้ง';
